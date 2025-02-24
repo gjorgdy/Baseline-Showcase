5d8c0257c40f0262a0730e150a1cdaa220f85c06
Jordy <jordyreins@gmail.com>
2025-02-24 02:33:03 +0100

    ADD: missing files

---
+++ Core/Baseline.cs
@@ -0,0 +1,15 @@
+﻿using Core.Interfaces;
+
+namespace Core;
+
+public class Baseline
+{
+
+    public static IMail MailService { get; private set; } = null!;
+
+    public Baseline(IMail mailService)
+    {
+        MailService = mailService;
+    }
+
+}
