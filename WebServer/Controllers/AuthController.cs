0399b47865414a5cafd72ede91458180c5795f0b
Jordy <jordyreins@gmail.com>
2025-03-11 21:48:40 +0100

    ADD: Google OAuth

--- WebServer/Controllers/AuthController.cs
+++ WebServer/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using WebServer.Models;
 
 namespace WebServer.Controllers;
 
+[Route("auth")]
 public class AuthController : Controller
 {
     // GET
@@ -11,13 +12,32 @@ public class AuthController : Controller
     {
         return View(AuthPageModel.GetInstance());
     }
+    
+    [Route("success")]
+    public IActionResult Success(string response)
+    {
+        return View(new AuthStatusModel(response));
+    }
 
-    // [Authorize]
-    public async Task<IActionResult> Success(string code)
+    [Route("callback/discord")]
+    public async Task<IActionResult> Discord(string code)
     {
         var auth = new DiscordAuth();
         _ = await auth.RequestTokens(code);
 
-        return View(new AuthStatusModel("Success: " + await auth.GetUserData()));
+        return RedirectToAction("Success", "Auth");
+        // return View(new AuthStatusModel("Success: " + await auth.GetUserData()));
     }
+
+    [Route("callback/google")]
+    public async Task<IActionResult> Google(string code)
+    {
+        await Console.Out.WriteLineAsync("Google Authenticator");
+        var auth = new GoogleAuth();
+        _ = await auth.RequestTokens(code);
+        
+        // return RedirectToAction("Success", "Auth");
+        return View(new AuthStatusModel(auth.GetUserSub() ?? "No Sub"));
+    }
+    
 }
--- WebServer/Models/AuthPageModel.cs
+++ WebServer/Models/AuthPageModel.cs
@@ -24,6 +24,12 @@ public class AuthPageModel
                 "https://discord.com/oauth2/authorize?client_id=1115260593176330241&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A7166%2Fauth%2Fsuccess&scope=identify+connections+guilds+openid"
             )
         );
+        _instance.Platforms.Add(
+            new AuthenticationPlatform(
+                "Google",
+                "https://accounts.google.com/o/oauth2/auth?scope=openid&response_type=code&access_type=offline&redirect_uri=https://localhost:44350/auth/callback/google&client_id=239855070043-9jmq31hrfh6gnhotvjvg9mlkeso8uv25.apps.googleusercontent.com"
+            )
+        );
         return _instance;
     }
 
